7eb1ac837761fb4425fdb80e8c6fe8840ef3fdf1
zume06 <hashizume.yuuka@g.sp.m.is.nagoya-u.ac.jp>
2023-11-23 18:41:24 +0900

    change residuals samples

--- blank.py
+++ blank.py
@@ -1,7 +1,7 @@
 for set in range(1, 8):
     dir = "./wav/set{}".format(set)
-    # for inst in ["drums", "bass", "piano", "guitar", "residuals"]:
-    for inst in ["residuals"]:
+    for inst in ["drums", "bass", "piano", "guitar"]:
+        # for inst in ["residuals"]:
         with open("{}/{}_X.list".format(dir, inst), mode="a") as f:
             f.write("0")
         with open("{}/{}_A.list".format(dir, inst), mode="a") as f:
@@ -10,8 +10,8 @@ for set in range(1, 8):
             f.write("0")
 for set in range(11, 18):
     dir = "./wav/set{}".format(set)
-    # for inst in ["drums", "bass", "piano", "guitar", "residuals"]:
-    for inst in ["residuals"]:
+    for inst in ["drums", "bass", "piano", "guitar"]:
+        # for inst in ["residuals"]:
         with open("{}/{}_X.list".format(dir, inst), mode="a") as f:
             f.write("0")
         with open("{}/{}_A.list".format(dir, inst), mode="a") as f:
